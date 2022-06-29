2db78a563e5dee37a67bac0e3ef1e15535269e1e
Teslagirl69 <teslagirl17@gmail.com>
2022-06-29 13:26:54 +0300

    add test for power up

--- hero_spec.rb
+++ hero_spec.rb
@@ -4,4 +4,10 @@ describe Hero do
     hero = Hero.new 'john'
     expect(hero.name).to eq 'John' # аналог hero.name = 'John'
     end
+
+
+     it "can power up " do
+    hero = Hero.new 'john'
+    expect(hero.power_up).to eq 110 # аналог hero.power_up = 110
+    end
 end
