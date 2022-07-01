09e0733338ab60772339798da2085353dd4850ee
Teslagirl69 <teslagirl17@gmail.com>
2022-07-01 18:48:56 +0300

    add power_down and hero_info tests

--- hero_spec.rb
+++ hero_spec.rb
@@ -10,4 +10,17 @@ describe Hero do
     hero = Hero.new 'john'
     expect(hero.power_up).to eq 110 # аналог hero.power_up = 110
     end
+
+
+     it "can power down " do
+    hero = Hero.new 'john'
+    expect(hero.power_down).to eq 90 # аналог hero.power_down = 90
+    end
+
+    it "displays full unfo " do
+    hero = Hero.new 'john'
+    expect(hero.hero_info).to eq 'John has 100 health' # аналог hero.power_down = 90
+    end
+
+
 end
